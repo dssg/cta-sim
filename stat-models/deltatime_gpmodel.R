cbb816460a65708b5a29c468894d4054902e8d68
PlustheMonk <dempsey.walter@gmail.com>
2013-07-08 20:51:46 +0000

    Added Basic Gaussian Process (brownian motion) model for arrival times with simulated dataset

--- stat-models/deltatime_gpmodel.R
+++ stat-models/deltatime_gpmodel.R
@@ -1,8 +1,10 @@
-### Simulated Delta Times for the Fake Bus Route with 5 Stops ###
+### Required Libraries ###
+library(rbugs)
+library(coda)
 
-Stops <- c(1,2,3,4,5)
+### Simulated Delta Times for the Fake Bus Route with 20 Evenly Spaced Stops ###
 
-time_dist <- c(5,10,5,15) # Distance in Time Between Stops
+time_dist <- rep(1,20) # Distance in Time Between Stops
 num_stops = length(time_dist)+1 # Number of Stops
 
 exp_arr_time <- rep(0,length(time_dist)) # Expected Arrival Time at Stop
@@ -15,29 +17,33 @@ simulate <- function(time_dist, sigma) { # Simulate Delta Times for a Given Time
   num_stops = length(time_dist)+1
   bus_times <- rep(0,num_stops) # Assume Delta Times are Conditional Normal, will adjust to be skewed normal
   for(i in 2:num_stops) {
-    # neg_portion <- rnorm(1,bus_times[i-1],sigma * time_dist[i-1])
+    #  neg_portion <- rnorm(1,bus_times[i-1],sigma * time_dist[i-1])
     neg_portion <- 0
     bus_times[i] <- rnorm(1,bus_times[i-1],sigma * time_dist[i-1]) + neg_portion * as.numeric(neg_portion < 0) 
   }
   return(bus_times)
 }
 
-sigma <- 0.2
-num_obs <- 100
+sigma <- 0.5
+1/sigma
+num_obs <- 1000
+
 simulation <- matrix(nrow = length(time_dist), ncol = num_obs)
 for (i in 1:num_obs) {
   simulation[,i] <- simulate(time_dist,sigma)[2:num_stops]
 }
 
 
-##### BUGS CODE ####
+### BUGS CODE - Estimate Single Variance Parameter ###
 
 model.str <- 'model
 {
   for(d in 1:num_obs) {
-    delta[1,d] ~ dnorm(0, itau2.alpha * diff_arrival[1])
+    delta[1,d] ~ dnorm(0, var[1,d])
+    var[1,d] <- itau2.alpha / diff_arrival[1]
     for(i in 2:(num_stops-1)) {
-      delta[i,d] ~ dnorm(delta[i-1], itau2.alpha * diff_arrival[i])  
+      delta[i,d] ~ dnorm( delta[i-1,d], var[i,d])
+      var[i,d] <- itau2.alpha / diff_arrival[i]
     }
   }
   itau2.alpha ~ dgamma(1,1)
@@ -47,7 +53,7 @@ model.file = file("gaussianprocess_model.bug")
 writeLines(model.str, model.file)
 close(model.file)
 
-data <- list("diff_arrival" = time_dist, "delta" = simulation)
+data <- list("diff_arrival" = time_dist, "delta" = simulation, "num_obs" = num_obs, "num_stops" = num_stops)
 
 inits <- list(list(itau2.alpha = rgamma(1,0.1,10)))
 
@@ -55,7 +61,7 @@ parameters <- c("itau2.alpha")
 
 load.sim <- rbugs(data, inits, parameters, "gaussianprocess_model.bug",
                   verbose=T,
-                  n.chains=1, n.iter=2000,
+                  n.chains=1, n.iter=6000, n.burnin = 1000, n.thin = 10,
                   bugsWorkingDir="/tmp", cleanBugsWorkingDir = T)
 
 load.mcmc <- rbugs2coda(load.sim)
