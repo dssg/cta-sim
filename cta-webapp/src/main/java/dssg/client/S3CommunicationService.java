df164b2248ab0c39f6883a6f0e4f3d61c3f725be
Andres Akle <andresakle@gmail.com>
2013-07-16 20:38:40 -0500

    Webapp: changed return type to List<MyStats> object for charts.

--- cta-webapp/src/main/java/dssg/client/S3CommunicationService.java
+++ cta-webapp/src/main/java/dssg/client/S3CommunicationService.java
@@ -1,11 +1,13 @@
 package dssg.client;
 
+import java.util.List;
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("s3Com")
 public interface S3CommunicationService extends RemoteService{
 		
-	public int uploadFile(String filename);
+	public List<MyStats> uploadFile(String filename);
 
 }
