36226106618f4661e340824fb546229a5cc47f24
Jordan T Bates <jtbates@gmail.com>
2013-07-24 22:47:27 +0000

    turn unquote.sh into generic batch apply script

---
+++ data-formatting/rcp/batch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run a command over all csv files in one directory and output to another
+# Usage:
+# $ ./batch.sh "your command" inputdir outpudir
+for file in "$2"/*.csv
+do
+filename=$(basename "$file")
+echo "$filename"
+"$1" < "$file" > "$3/$filename"
+done
