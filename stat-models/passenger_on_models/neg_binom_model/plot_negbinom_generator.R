91f2c1ebf6672c895d237490ec923e1db49e3089
PlustheMonk <dempsey.walter@gmail.com>
2013-08-01 20:15:40 +0000

    Updated Plot of Avgs for Redshift and Full Day

--- stat-models/passenger_on_models/neg_binom_model/plot_negbinom_generator.R
+++ stat-models/passenger_on_models/neg_binom_model/plot_negbinom_generator.R
@@ -1,22 +1,26 @@
 # Turn Command Line Arguments On
 
-# Command Line: Rscript distr_negbinom_generator.R /home/wdempsey/dssg-cta-project/stat-models/mcmc_output/avgsim_negbinom_on.csv
+# Command Line: Rscript plot_negbinom_generator.R /home/wdempsey/dssg-cta-project/stat-models/passenger_on_models/neg_binom_model//mcmc_output/avgsim_negbinom_on.csv 1 1 
 
 args <- commandArgs(TRUE)
 parameter_pathname <- toString(args[1]) # Model Parameters
 month <- as.numeric(args[2])
 weekend <- as.numeric(args[3])
 
+if(file.exists(paste("/home/wdempsey/dssg-cta-project/stat-models/passenger_on_models/neg_binom_model/distr_by_month_week/distr_on_mon_",month,"_week_",weekend,".csv", sep = ""))) {
+
 ### Loading and Cleaning the Data and Parameter Estimates ###
+
+print(parameter_pathname)
 
 params = read.csv(parameter_pathname, header = TRUE)
 
 ### Construct Explanatory Variables for Given Observation ###
 
-alpha = as.numeric(params[1:47])
-beta = as.numeric(params[48:49])
-rho = as.numeric(params[50:96])
-gamma = as.numeric(params[97:108])
+alpha = as.numeric(params[1:48])
+beta = as.numeric(params[49:50])
+rho = as.numeric(params[51:98])
+gamma = as.numeric(params[99:110])
 
 halfhr = seq(0,24,0.5)
 
@@ -34,8 +38,31 @@ for(i in 1:length(mean)) {
     lowerquartile[i] = qnbinom(0.25, mu = mean[i], size = rho[i])
     upperquartile[i] = qnbinom(0.75, mu = mean[i], size = rho[i])
 }
+
 mean = as.numeric(mean)
 
-X = cbind(lowerquartile, round(mean,2), upperquartile)
+negbinom_distr = cbind(lowerquartile, round(mean,2), upperquartile)
 
-write.table(X, "distr_negbinom_stop.csv", sep=",", row.names = FALSE, col.names = FALSE)
+write.table(negbinom_distr, paste("/home/wdempsey/dssg-cta-project/stat-models/passenger_on_models/neg_binom_model/distr_by_month_week/est_distr_on_mon_",month,"_week_",weekend,".csv", sep = ""), sep=",", row.names = FALSE, col.names = FALSE)
+
+actual_distr = read.csv(paste("/home/wdempsey/dssg-cta-project/stat-models/passenger_on_models/neg_binom_model/distr_by_month_week/distr_on_mon_",month,"_week_",weekend,".csv", sep = ""), header = FALSE)
+
+halfhr = seq(0,24, 0.5)
+
+shownobs = which(halfhr > 5 & halfhr < 23)
+
+
+png(paste('avg_prediction_',month,'_',weekend,'.png', sep = ""), width = 1100, height = 750)
+plot(halfhr[shownobs], actual_distr[shownobs,2], lty = 1, type = "l", axes = FALSE, xlab = "Hour", ylab = "Passenger On Count", ylim = c(0,45))
+axis(2)
+axis(1, at = seq(5,23,2))
+lines(halfhr[shownobs],actual_distr[shownobs,1], lty = 2)
+lines(halfhr[shownobs],actual_distr[shownobs,3], lty = 2)
+lines(halfhr[shownobs],negbinom_distr[shownobs,2], lty = 1, col = "red")
+lines(halfhr[shownobs],negbinom_distr[shownobs,1], lty = 2, col = "red")
+lines(halfhr[shownobs],negbinom_distr[shownobs,3], lty = 2, col = "red")
+mtext(paste("Distrubtion of Avg. Fitted and Observed Data for Month",month,"and Week/Weekday",as.numeric(weekend==2)), line = 0)
+legend(7,35, c("Observed Mean", "Observed 75th/25th Percentile","Predicted Mean", "Predicted 75th/25th Percentile"), lty = c(1,2,1,2), col = c("black","black", "red", "red"), cex = 0.75)
+dev.off()
+
+}
