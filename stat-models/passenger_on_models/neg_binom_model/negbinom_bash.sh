6406fc391b8989e838247187c27373fc7f8eb1c6
PlustheMonk <dempsey.walter@gmail.com>
2013-08-01 20:29:20 +0000

    Updated Bash File to reflect change to Redshift and Full Day

--- stat-models/passenger_on_models/neg_binom_model/negbinom_bash.sh
+++ stat-models/passenger_on_models/neg_binom_model/negbinom_bash.sh
@@ -2,16 +2,16 @@
 
 echo "Running the Model Fitting via MCMC"
 
-Rscript >& /dev/null passengeron_negbin_model.R ${3:-s3://dssg-cta-data/rcp_join2/train/apc/taroute=6/direction_name=South/stop_id=1984} ${4:-./mcmc_output/totalsim_negbinom_on.csv} ${5:-./mcmc_output/avgsim_negbinom_on.csv} ${1:-1} ${2-1}
+Rscript >& /dev/null passengeron_negbin_model.R ${3:-6} ${4:-0} ${5:-1423} ${6:-/home/wdempsey/dssg-cta-project/stat-models/passenger_on_models/neg_binom_model/mcmc_output/totalsim_negbinom_on.csv} ${7:-/home/wdempsey/dssg-cta-project/stat-models/passenger_on_models/neg_binom_model/mcmc_output/avgsim_negbinom_on.csv}
 
 echo "Finished Fitting! Onto Validation"
 
-Rscript validation_negbinom_on.R ${5:-./mcmc_output/avgsim_negbinom_on.csv} ${3:-s3://dssg-cta-data/rcp_join2/test/apc/taroute=6/direction_name=South/stop_id=1984}
+Rscript validation_negbinom_on.R ${7:-/home/wdempsey/dssg-cta-project/stat-models/passenger_on_models/neg_binom_model/mcmc_output/avgsim_negbinom_on.csv} ${3:-6} ${4:-0} ${5:-1423}
 
 echo "Validation Complete!  Onto Distribution Output and Visualization"
 
-Rscript >& /dev/null plot_negbinom_generator.R ${5:-./mcmc_output/avgsim_negbinom_on.csv} ${1:-1} ${2:-1}
+Rscript >& /dev/null plot_negbinom_generator.R ${7:-/home/wdempsey/dssg-cta-project/stat-models/passenger_on_models/neg_binom_model/mcmc_output/avgsim_negbinom_on.csv} ${1:-1} ${2:-1}
 
-Rscript &> /dev/null plot_mcmc.R ${2:-./mcmc_output/totalsim_negbinom_on.csv} ${1:-1} ${2:-1}
+# Rscript &> /dev/null plot_mcmc.R ${2:-./mcmc_output/totalsim_negbinom_on.csv} ${1:-1} ${2:-1}
 
 mv total_prediction_* avg_prediction_* mcmc_plots/
